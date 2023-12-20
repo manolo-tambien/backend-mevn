3a81924ee9df158be68deced2204682f7cdf9ce1
= <=>
2023-12-19 20:40:59 -0600

    Se agrega endpoint para editar cuenta espejo

--- controllers/cuentasEspejoController.js
+++ controllers/cuentasEspejoController.js
@@ -1,7 +1,8 @@
- 
+
 import CuentasEspejo from "../models/CuentaEspejo.js";
- 
- 
+import { validateObjectId, handleNotFoundError, formatDate } from '../utils/index.js'
+
+
 
 const getAllCuentasEspejo = async (req, res) => {
     try {
@@ -12,10 +13,51 @@ const getAllCuentasEspejo = async (req, res) => {
         console.log(error)
     }
 }
- 
 
-  
+const updateCuentaEspejo = async (req, res) => {
+    const { id } = req.params
+
+    // Validar por object id
+    if (validateObjectId(id, res)) return
+
+    const cuentaEspejo = await CuentasEspejo.findById(id)
+    if (!cuentaEspejo) {
+        return handleNotFoundError('La cuenta espejo no existe', res)
+    }
+
+    const {
+        gerente,
+        cliente,
+        linea_transportista,
+        salidas, 
+        url,
+        usuario,
+        cuenta,
+        estaciones
+    } = req.body
+    
+    cuentaEspejo.gerente = gerente
+    cuentaEspejo.cliente = cliente
+    cuentaEspejo.linea_transportista = linea_transportista
+    cuentaEspejo.salidas = salidas
+    cuentaEspejo.url = url
+    cuentaEspejo.usuario = usuario
+    cuentaEspejo.cuenta = cuenta
+    cuentaEspejo.estaciones = estaciones
+
+    try {
+        const result = await cuentaEspejo.save()
+
+        res.json({
+            msg: 'Cuenta espejo actualizada'
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 
 export {
-    getAllCuentasEspejo
+    getAllCuentasEspejo,
+    updateCuentaEspejo
 }
--- routes/cuentasEspejoRoutes.js
+++ routes/cuentasEspejoRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express'
-import {getAllCuentasEspejo} from '../controllers/cuentasEspejoController.js'
+import {getAllCuentasEspejo, updateCuentaEspejo} from '../controllers/cuentasEspejoController.js'
 import authMiddleware from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.route('/')
     .get(getAllCuentasEspejo)
+
+router.route('/:id')
+    .put( updateCuentaEspejo)
     
 export default router
