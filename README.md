aeb779b26d855fd2100effdf6d15bd2b3f67d52e
= <=>
2024-03-11 22:43:36 -0600

    MD updated

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ Backend made of:
 - Node
 - Express
 - Moongose
+
+## How to use
+### Install mongo on Docker
+```
+docker pull mongo
+docker run --name mongo-db -d -p 27017:27017 mongo
+docker ps
+```
--- config/db.js
+++ config/db.js
@@ -1,13 +1,35 @@
-import mongoose from 'mongoose'
-import colors from 'colors'
+import mongoose from 'mongoose';
+import colors from 'colors';
 
 export const db = async () => {
     try {
-        const db = await mongoose.connect(process.env.MONGO_URI)
-        const url = `${db.connection.host}:${db.connection.port}`
-        console.log( colors.cyan(`MongoDB se conectó correctamente: ${url}`))
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        const db = mongoose.connection;
+        const url = `${db.host}:${db.port}`;
+        console.log(colors.cyan(`MongoDB se conectó correctamente: ${url}`));
+
+        // Verificar si la base de datos y la colección existen, si no, crearlas
+        const adminDb = db.useDb('admin');
+        const databases = await adminDb.admin().listDatabases();
+        const dbExists = databases.databases.some(db => db.name === 'mi_base_de_datos');
+
+        if (!dbExists) {
+            await adminDb.db('mi_base_de_datos').createCollection('mi_coleccion');
+            console.log(colors.green('Base de datos y colección creadas correctamente'));
+        } else {
+            const collections = await adminDb.db('mi_base_de_datos').listCollections().toArray();
+            const collectionExists = collections.some(col => col.name === 'mi_coleccion');
+            if (!collectionExists) {
+                await adminDb.db('mi_base_de_datos').createCollection('mi_coleccion');
+                console.log(colors.green('Colección creada correctamente'));
+            }
+        }
     } catch (error) {
-        console.log(`Error: ${error.message}`)
-        process.exit(1)
+        console.error(colors.red(`Error: ${error.message}`));
+        process.exit(1);
     }
-}
+};
