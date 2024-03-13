774d4209a9726c2bb62ee712b618bfa7fda62def
= <=>
2024-03-12 22:34:29 -0600

    Added parameter to name the database

--- config/db.js
+++ config/db.js
@@ -1,35 +1,16 @@
-import mongoose from 'mongoose';
-import colors from 'colors';
+import mongoose from 'mongoose'
+import colors from 'colors'
 
 export const db = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-
-        const db = mongoose.connection;
-        const url = `${db.host}:${db.port}`;
-        console.log(colors.cyan(`MongoDB se conectó correctamente: ${url}`));
-
-        // Verificar si la base de datos y la colección existen, si no, crearlas
-        const adminDb = db.useDb('admin');
-        const databases = await adminDb.admin().listDatabases();
-        const dbExists = databases.databases.some(db => db.name === 'mi_base_de_datos');
-
-        if (!dbExists) {
-            await adminDb.db('mi_base_de_datos').createCollection('mi_coleccion');
-            console.log(colors.green('Base de datos y colección creadas correctamente'));
-        } else {
-            const collections = await adminDb.db('mi_base_de_datos').listCollections().toArray();
-            const collectionExists = collections.some(col => col.name === 'mi_coleccion');
-            if (!collectionExists) {
-                await adminDb.db('mi_base_de_datos').createCollection('mi_coleccion');
-                console.log(colors.green('Colección creada correctamente'));
-            }
-        }
+        // Added the parameter dbName for specify the name of the database when the script runs
+        const db = await mongoose.connect(process.env.MONGO_URI,{
+            dbName: 'CuentasEspejoDB'
+        })
+        const url = `${db.connection.host}:${db.connection.port}`
+        console.log( colors.cyan(`MongoDB se conectó correctamente: ${url}`))
     } catch (error) {
-        console.error(colors.red(`Error: ${error.message}`));
-        process.exit(1);
+        console.log(`Error: ${error.message}`)
+        process.exit(1)
     }
-};
+}
